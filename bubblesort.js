6450fa6ce3dac5299bfeb4a5787f4fdc2e3131e9
E N J A <enja.reyes@gmail.com>
2015-05-21 19:50:12 -0500

    Update bubblesort.js

--- bubblesort.js
+++ bubblesort.js
@@ -1,43 +1,42 @@
+// Bubble sort is one of the simplest sorting algorithms to understand and implement.
 
-Bubble sort is one of the simplest sorting algorithms to understand and implement.
+// Time Complexity: O(n2) - its efficiency decreases dramatically on lists of more than a small number of elements. 
+// There exist many sorting algorithms with much better worst-case or average time complexity. 
+// Even other О(n2) sorting algorithms (like insertion sort) tend to have better performance than bubble sort. 
 
-Time Complexity: O(n2) - its efficiency decreases dramatically on lists of more than a small number of elements. 
-There exist many sorting algorithms with much better worst-case or average time complexity. 
-Even other О(n2) sorting algorithms (like insertion sort) tend to have better performance than bubble sort. 
+// Basic Idea: 
+// Steps through the list to be sorted
+// Compares each pair of adjacent items and swaps them if they are in the wrong order. 
+// The pass through the list is repeated until no swaps are needed
 
-Basic Idea: 
-Steps through the list to be sorted
-Compares each pair of adjacent items and swaps them if they are in the wrong order. 
-The pass through the list is repeated until no swaps are needed
+// Example: 
 
-Example: 
+// Let us take the array of numbers "5 1 4 2 8", and sort the array from lowest number to 
+// greatest number using bubble sort. In each step, elements written in bold are being compared. 
+// Three passes will be required.
 
-Let us take the array of numbers "5 1 4 2 8", and sort the array from lowest number to 
-greatest number using bubble sort. In each step, elements written in bold are being compared. 
-Three passes will be required.
+// First Pass:
+// ( 5 1 4 2 8 ) \to ( 1 5 4 2 8 ) Here, algorithm compares the first two elements, and swaps since 5 > 1.
+// ( 1 5 4 2 8 ) \to ( 1 4 5 2 8 ) Swap since 5 > 4
+// ( 1 4 5 2 8 ) \to ( 1 4 2 5 8 ) Swap since 5 > 2
+// ( 1 4 2 5 8 ) \to ( 1 4 2 5 8 ) Since these elements are already in order (8 > 5), they don't swap.
 
-First Pass:
-( 5 1 4 2 8 ) \to ( 1 5 4 2 8 ) Here, algorithm compares the first two elements, and swaps since 5 > 1.
-( 1 5 4 2 8 ) \to ( 1 4 5 2 8 ) Swap since 5 > 4
-( 1 4 5 2 8 ) \to ( 1 4 2 5 8 ) Swap since 5 > 2
-( 1 4 2 5 8 ) \to ( 1 4 2 5 8 ) Since these elements are already in order (8 > 5), they don't swap.
+// Second Pass:
+// ( 1 4 2 5 8 ) \to ( 1 4 2 5 8 )
+// ( 1 4 2 5 8 ) \to ( 1 2 4 5 8 ), Swap since 4 > 2
+// ( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
+// ( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
+// Now, the array is already sorted, but the algorithm does not know if it is completed. 
+// The algorithm needs one whole pass without any swap to know it is sorted.
 
-Second Pass:
-( 1 4 2 5 8 ) \to ( 1 4 2 5 8 )
-( 1 4 2 5 8 ) \to ( 1 2 4 5 8 ), Swap since 4 > 2
-( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
-( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
-Now, the array is already sorted, but the algorithm does not know if it is completed. 
-The algorithm needs one whole pass without any swap to know it is sorted.
-
-Third Pass:
-( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
-( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
-( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
-( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
+// Third Pass:
+// ( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
+// ( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
+// ( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
+// ( 1 2 4 5 8 ) \to ( 1 2 4 5 8 )
 
 
-To Do: Write a function that takes an array of integers and uses bubble sort to return a sorted array.
+// To Do: Write a function that takes an array of integers and uses bubble sort to return a sorted array.
 
 
 function bubbleSort (array){
