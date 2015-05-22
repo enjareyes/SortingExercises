c5455eef9c0d928724a06f5cf8e5a4a9aac9ad7b
E N J A <enja.reyes@gmail.com>
2015-05-21 19:55:09 -0500

    Update quicksort.js

--- quicksort.js
+++ quicksort.js
@@ -1,21 +1,20 @@
 
-// Quicksort is a comparison sort, meaning that it can sort items of any type 
-// for which a "less-than" relation (formally, a total order) is defined.
+// Quicksort is a comparison sort, meaning it can sort items of any type 
+// where a 'less than' relation is defined.
 
 // Time Complexity: 
 // Best case: O(n log n) 
 // Average:  O(n log n) 
-// Worst case: O(n2) - this behavior is rare.
+// Worst case: O(n2) - this is rare.
 
 // Basic Idea: 
-// Pick one element in the array, which will be the pivot.
-// Make one pass through the array, called a partition step, re-arranging the entries so that:
-// the pivot is in its proper place.
-// entries smaller than the pivot are to the left of the pivot.
-// entries larger than the pivot are to its right.
-// Recursively apply quicksort to the part of the array that is to the left of the pivot, 
+// 1. Pick one element in the array, which will be the pivot.
+// 2. Make one pass through the array, re-arranging the entries so:
+  // the pivot is in its proper place.
+  // entries smaller than the pivot are to the left of the pivot.
+  // entries larger than the pivot are to its right.
+// 3. Recursively apply quicksort to the part of the array left of the pivot, 
 // and to the right part of the array.
-// Here we don't have the merge step, at the end all the elements are in the proper order.
 
 // To Do: Write a function that takes an array of integers and uses quick sort to return a sorted array.
 
